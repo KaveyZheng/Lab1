23000ac8f72094b6f339949c924476aad4ec99ef
KaveyZheng <kaveyzheng1@gmail.com>
2022-09-14 21:45:49 -0400

    Commit optional functions

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -12,14 +12,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var backLabel: UILabel!
     @IBOutlet weak var frontLabel: UILabel!
     
+    @IBOutlet weak var ans1: UILabel!
+    @IBOutlet weak var ans2: UILabel!
+    @IBOutlet weak var ans3: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func didTapOnFlashCard(_ sender: Any) {
-        frontLabel.isHidden = true
+        if (frontLabel.isHidden == false) {
+            frontLabel.isHidden = true
+            
+            ans1.isHidden = true
+            ans2.isHidden = true
+            
+            ans3.backgroundColor = .systemGreen
+            ans3.textColor = .black
+        } else {
+            frontLabel.isHidden = false
+            
+            ans1.isHidden = false
+            ans2.isHidden = false
+            
+            ans3.backgroundColor = .systemBlue
+            ans3.textColor = .white
+        }
     }
-    
 }
 
